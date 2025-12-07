fc5c9b6541b1ffecc84a12060e7e2454f26c79f1
Luca Yang <2655226019@qq.com>
2025-12-07 19:08:13 +0800

    chore: 调整缓存参数

--- cache.py
+++ cache.py
@@ -1,6 +1,6 @@
 from expiringdict import ExpiringDict
 
-cache = ExpiringDict(max_len=1000, max_age_seconds=60*60)
+cache = ExpiringDict(max_len=100000, max_age_seconds=60*60*4)
 
 def get_cache(key):
     return cache.get(key)
