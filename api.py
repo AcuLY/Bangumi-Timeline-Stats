974e115dd93141e1805a402878b22183b5b235e5
Luca Yang <2655226019@qq.com>
2025-12-21 18:33:27 +0800

    perf: 调整爬虫速度，延长缓存时间

--- api.py
+++ api.py
@@ -5,37 +5,52 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 headers = {
-    'User-Agent': 'AcuL/Bangumi-Timeline-Stats/1.0 (Web) (https://github.com/AcuLY/Bangumi-Timeline-Stats)'
+    "User-Agent": "AcuL/Bangumi-Timeline-Stats/1.0 (Web) (https://github.com/AcuLY/Bangumi-Timeline-Stats)"
 }
+timeout = httpx.Timeout(
+    connect=5.0,
+    read=30.0,
+    write=10.0,
+    pool=30.0,
+)
+
 
 def user_url(user_id: str) -> str:
-    return f'https://bangumi.tv/user/{user_id}/timeline'
+    return f"https://bangumi.tv/user/{user_id}/timeline"
+
 
 def parse_datetime(response: httpx.Response) -> list[datetime]:
-    response.encoding = 'utf-8'
+    response.encoding = "utf-8"
     html_content = response.text
-    
-    soup = BeautifulSoup(html_content, 'html.parser')
-    
+
+    soup = BeautifulSoup(html_content, "html.parser")
+
     if not soup.find(id="timeline"):
         return []
-    
-    elements = soup.find_all(class_='titleTip')
-    timelines = [element.get('title') for element in elements]
-    datetimes = [datetime.strptime(timeline, "%Y-%m-%d %H:%M") for timeline in timelines]
+
+    elements = soup.find_all(class_="titleTip")
+    timelines = [element.get("title") for element in elements]
+    datetimes = [
+        datetime.strptime(timeline, "%Y-%m-%d %H:%M") for timeline in timelines
+    ]
 
     return datetimes
 
 
 async def fetch(client: httpx.AsyncClient, url: str, params: dict):
-    response = await client.get(url, params=params)
     await asyncio.sleep(0.1)
+    response = await client.get(url, params=params)
     return response
 
 
-async def fetch_timelines_by_pages(client: httpx.AsyncClient, user_id: str, pages: int) -> list[datetime]:
+async def fetch_timelines_by_pages(
+    client: httpx.AsyncClient, user_id: str, pages: int
+) -> list[datetime]:
     url = user_url(user_id)
-    tasks = [fetch(client, url, {'type': 'progress', 'page': page+1}) for page in range(pages)]
+    tasks = [
+        fetch(client, url, {"type": "progress", "page": page + 1})
+        for page in range(pages)
+    ]
     responses = await asyncio.gather(*tasks)
     datetimes = []
     for response in responses:
@@ -43,12 +58,18 @@ async def fetch_timelines_by_pages(client: httpx.AsyncClient, user_id: str, page
     return datetimes
 
 
-async def fetch_timelines_by_datetime(client: httpx.AsyncClient, user_id: str, due_date: datetime, step: int) -> list[datetime]:
+async def fetch_timelines_by_datetime(
+    client: httpx.AsyncClient, user_id: str, due_date: datetime, step: int
+) -> list[datetime]:
     datetimes = []
     url = user_url(user_id)
     begin_page = 1
     while True:
-        tasks = tasks = [fetch(client, url, {'type': 'progress', 'page': page}) for page in range(begin_page, begin_page + step)]
+        await asyncio.sleep(0.2)
+        tasks = tasks = [
+            fetch(client, url, {"type": "progress", "page": page})
+            for page in range(begin_page, begin_page + step)
+        ]
         responses = await asyncio.gather(*tasks)
 
         is_end = False
@@ -67,36 +88,40 @@ async def fetch_timelines_by_datetime(client: httpx.AsyncClient, user_id: str, d
                 break
         if is_end:
             return datetimes
-        
+
         begin_page += step
-        await asyncio.sleep(0.2)
 
 
 async def fetch_hours(user_id: str, fetch_range: str) -> list[int]:
-    async with httpx.AsyncClient(headers=headers, limits=httpx.Limits(max_connections=5)) as client:
+    async with httpx.AsyncClient(
+        headers=headers, limits=httpx.Limits(max_connections=5), timeout=timeout
+    ) as client:
         # 数量范围只包含数字
         if fetch_range.isdecimal():
-            datetimes = await fetch_timelines_by_pages(client, user_id, int(fetch_range))
+            datetimes = await fetch_timelines_by_pages(
+                client, user_id, int(fetch_range)
+            )
         else:
             # time_range 的格式为 %d%c 如 7d, 30d, 6m, 1y
             value = int(fetch_range[:-1])
             time_type = fetch_range[-1]
             now = datetime.now()
-            if time_type == 'd':
+            if time_type == "d":
                 due_date = now - relativedelta(days=value)
                 step = 1
-            elif time_type == 'm':
+            elif time_type == "m":
                 due_date = now - relativedelta(months=value)
                 step = 2
             else:
                 due_date = now - relativedelta(years=value)
                 step = 5
 
-            datetimes = await fetch_timelines_by_datetime(client, user_id, due_date, step)
+            datetimes = await fetch_timelines_by_datetime(
+                client, user_id, due_date, step
+            )
 
         hours = [0] * 24
         for dt in datetimes:
             hours[dt.hour] += 1
 
         return hours
-
--- cache.py
+++ cache.py
@@ -1,6 +1,6 @@
 from expiringdict import ExpiringDict
 
-cache = ExpiringDict(max_len=100000, max_age_seconds=60*60*4)
+cache = ExpiringDict(max_len=100000, max_age_seconds=60*60*12)
 
 def get_cache(key):
     return cache.get(key)
